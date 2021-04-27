10a685af4acfe5f3b5eba937dd31728109eff93b
Thomas Paulin <7918983+thomaspaulin@users.noreply.github.com>
2021-04-27 11:04:19 +0200

    Logging improvements

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
+	"os/user"
+	"path"
 	"sync"
 	"time"
 )
@@ -19,12 +21,24 @@ type payload struct {
 	Links []string `json:"links"`
 }
 
+func logFile() (*os.File, error) {
+	u, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+	// todo figure out permissions for logging in /var/log/archive-box-bridge
+	p := path.Join(u.HomeDir, "archive-box-bridge.log")
+	f, err := os.OpenFile(p, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	return f, err
+}
+
 // Credit to https://stackoverflow.com/questions/18361750/correct-approach-to-global-logging-in-golang
 func createLogger() *log.Logger {
-	filename := "archive-box-bridge.log"
-	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := logFile()
 	if err != nil {
-		log.Fatalf("Error opening log file: %v", err)
+		panic(fmt.Sprintf("Error opening log file: %v", err))
+	} else {
+		log.Printf("Logging to %s", f.Name())
 	}
 	mw := io.MultiWriter(f, os.Stdout)
 	return log.New(mw, "[archive-box-bridge] ", log.Ldate|log.Ltime)
